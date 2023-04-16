99a9b240ce07150520b00f5ae40f4564825bb08e
rhettre <rhettre@gmail.com>
2023-04-15 22:27:33 -0500

    setup.py changes

--- setup.py
+++ setup.py
@@ -1,30 +1,27 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read()
+
 setup(
     name='coinbase-advancedtrade-python',
     version='0.1.0',
     description='The unofficial Python client for the Coinbase Advanced Trade API',
     author='Rhett Reisman',
     author_email='rhett@rhett.blog',
+    license='MIT',
     url='https://github.com/rhettre/coinbase-advancedtrade-python',
     packages=find_packages(),
-    install_requires=[
-        'http.client',
-        'hmac',
-        'hashlib',
-        'json',
-        'time',
-        'typing',
-        'enum',
-        'datetime',
-        'uuid',
-    ],
+    install_requires=requirements,
     keywords=['gdax', 'gdax-api', 'cbpro', 'cbpro-api', 'orderbook', 'trade', 'bitcoin', 'ethereum', 'BTC', 'ETH',
               'client', 'api', 'wrapper', 'exchange', 'crypto', 'currency', 'trading', 'trading-api', 'coinbase',
               'advanced-trade', 'prime', 'coinbaseadvancedtrade', 'coinbase-advanced-trade'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Information Technology',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
