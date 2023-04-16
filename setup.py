d81a45aa3b7186c0f1348ccd71196cb2635713f8
rhettre <rhettre@gmail.com>
2023-04-15 23:06:58 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name='coinbase-advancedtrade-python',
-    version='0.1.1',
+    version='0.1.2',
     description='The unofficial Python client for the Coinbase Advanced Trade API',
     long_description=long_description,
     long_description_content_type="text/markdown",
