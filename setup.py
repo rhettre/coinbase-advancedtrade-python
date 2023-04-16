4e9bbe395c8895e385583b996374c6e894e68bec
rhettre <rhettre@gmail.com>
2023-04-15 22:32:48 -0500

    setup.py changes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
-    requirements = f.read().splitlines
+    requirements = f.read().splitlines()
 
 setup(
     name='coinbase-advancedtrade-python',
