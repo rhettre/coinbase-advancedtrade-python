0834928baccc4a8be3eeec7ee374c34b1bc3fdd0
rhettre <rhettre@gmail.com>
2023-04-15 22:00:35 -0500

    added test scripts refactored project structure

---
+++ coinbase_advanced_trader/cb_auth.py
@@ -0,0 +1,47 @@
+import http.client
+import hmac
+import hashlib
+import json
+import time
+from typing import Dict
+from coinbase_advanced_trader.config import API_KEY, API_SECRET
+
+
+class CBAuth:
+    def __init__(self):
+        self.key = API_KEY
+        self.secret = API_SECRET
+
+    def __call__(self, method: str, path: str, body: str = '', params: Dict[str, str] = None) -> Dict:
+        if params:
+            query_params = '&'.join([f'{k}={v}' for k, v in params.items()])
+            path = f'{path}?{query_params}'
+
+        with http.client.HTTPSConnection("api.coinbase.com") as conn:
+            timestamp = str(int(time.time()))
+            message = timestamp + method.upper() + \
+                path.split('?')[0] + str(body)
+            signature = hmac.new(self.secret.encode(
+                'utf-8'), message.encode('utf-8'), digestmod=hashlib.sha256).hexdigest()
+
+            headers = {
+                "accept": "application/json",
+                "CB-ACCESS-KEY": self.key,
+                "CB-ACCESS-SIGN": signature,
+                "CB-ACCESS-TIMESTAMP": timestamp
+            }
+
+            conn.request(method, path, body, headers)
+            res = conn.getresponse()
+            data = res.read()
+
+            if res.status == 401:
+                print("Error: Unauthorized. Please check your API key and secret.")
+                return None
+
+            try:
+                response_data = json.loads(data.decode("utf-8"))
+                return response_data
+            except json.JSONDecodeError:
+                print("Error: Unable to decode JSON response. Raw response data:", data)
+                return None
---
+++ coinbase_advanced_trader/coinbase_client.py
@@ -0,0 +1,89 @@
+from enum import Enum
+from datetime import datetime
+import uuid
+import json
+from coinbase_advanced_trader.cb_auth import CBAuth
+from coinbase_advanced_trader.config import API_KEY, API_SECRET
+
+
+class Side(Enum):
+    BUY = 1
+    SELL = 0
+
+
+class Method(Enum):
+    POST = "POST"
+    GET = "GET"
+
+
+def generate_client_order_id():
+    return uuid.uuid4()
+
+
+cb_auth = CBAuth()
+
+
+def listAccounts(limit=49, cursor=None):
+    return cb_auth(Method.GET.value, '/api/v3/brokerage/accounts', params={'limit': limit, 'cursor': cursor})
+
+
+def getAccount(account_uuid):
+    return cb_auth(Method.GET.value, f'/api/v3/brokerage/accounts/{account_uuid}')
+
+
+def createOrder(client_order_id, product_id, side, order_configuration):
+    body = json.dumps({
+        "client_order_id": client_order_id,
+        "product_id": product_id,
+        "side": side,
+        "order_configuration": order_configuration
+    })
+    return cb_auth(Method.POST.value, '/api/v3/brokerage/orders', body)
+
+
+def cancelOrders(order_ids):
+    body = json.dumps({"order_ids": order_ids})
+    return cb_auth(Method.POST.value, '/api/v3/brokerage/orders/batch_cancel', body)
+
+
+def listOrders(**kwargs):
+    return cb_auth(Method.GET.value, '/api/v3/brokerage/orders/historical/batch', params=kwargs)
+
+
+def listFills(**kwargs):
+    return cb_auth(Method.GET.value, '/api/v3/brokerage/orders/historical/fills', params=kwargs)
+
+
+def getOrder(order_id):
+    return cb_auth(Method.GET.value, f'/api/v3/brokerage/orders/historical/{order_id}')
+
+
+def listProducts(**kwargs):
+    return cb_auth(Method.GET.value, '/api/v3/brokerage/products', params=kwargs)
+
+
+def getProduct(product_id):
+    return cb_auth(Method.GET.value, f'/api/v3/brokerage/products/{product_id}')
+
+
+def getProductCandles(product_id, start, end, granularity):
+    params = {
+        'start': start,
+        'end': end,
+        'granularity': granularity
+    }
+    return cb_auth(Method.GET.value, f'/api/v3/brokerage/products/{product_id}/candles', params=params)
+
+
+def getMarketTrades(product_id, limit):
+    return cb_auth(Method.GET.value, f'/api/v3/brokerage/products/{product_id}/ticker', params={'limit': limit})
+
+
+def getTransactionsSummary(start_date, end_date, user_native_currency='USD', product_type='SPOT'):
+    params = {
+        'start_date': start_date.strftime('%Y-%m-%dT%H:%M:%SZ'),
+        'end_date': end_date.strftime('%Y-%m-%dT%H:%M:%SZ'),
+        'user_native_currency': user_native_currency,
+        'product_type': product_type
+    }
+    return cb_auth(Method.GET.value, '/api/v3/brokerage/transaction_summary', params=params)
