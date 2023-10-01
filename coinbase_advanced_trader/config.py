38912398f642b96c32c8ad3fb8ee0f7eae6af1ec
rhettre <rhettre@gmail.com>
2023-10-01 13:05:45 -0500

    Fear Greed updates, UX improvements

--- coinbase_advanced_trader/config.py
+++ coinbase_advanced_trader/config.py
@@ -8,6 +8,22 @@ API_SECRET = None
 BUY_PRICE_MULTIPLIER = 0.995
 SELL_PRICE_MULTIPLIER = 1.005
 
+# Default schedule for the trade_based_on_fgi_simple function
+SIMPLE_SCHEDULE = [
+    {'threshold': 20, 'factor': 1.2, 'action': 'buy'},
+    {'threshold': 80, 'factor': 0.8, 'action': 'sell'}
+]
+
+# Default schedule for the trade_based_on_fgi_pro function
+PRO_SCHEDULE = [
+    {'threshold': 10, 'factor': 1.5, 'action': 'buy'},
+    {'threshold': 20, 'factor': 1.3, 'action': 'buy'},
+    {'threshold': 30, 'factor': 1.1, 'action': 'buy'},
+    {'threshold': 70, 'factor': 0.9, 'action': 'sell'},
+    {'threshold': 80, 'factor': 0.7, 'action': 'sell'},
+    {'threshold': 90, 'factor': 0.5, 'action': 'sell'}
+]
+
 
 def set_api_credentials(api_key=None, api_secret=None):
     global API_KEY
