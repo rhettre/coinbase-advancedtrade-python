e63c48a9c43ace35d20fc018d8f3dc84224c1293
David <davidmkcodes@gmail.com>
2023-12-04 12:52:02 -0800

    Update main.py starter requests to listAccounts and listPortfolios

--- coinbase_advanced_trader/main.py
+++ coinbase_advanced_trader/main.py
@@ -1,18 +1,17 @@
-from coinbase_client import listAccounts, listPortfolios, getPortfolioBreakdown
+from coinbase_client import (
+    listAccounts,
+    listPortfolios
+)
 from config import set_api_credentials
+
 def main():
     set_api_credentials()
 
-    #List accounts
+    # List accounts
     print(listAccounts())
 
     #ListPortfolios
-    portfolioList = listPortfolios()
-    print(portfolioList)
-
-    #Portfolio breakdown (of first portfolio)
-    portfolio_uuid = portfolioList['portfolios'][0]['uuid']
-    print(getPortfolioBreakdown(portfolio_uuid))
+    print(listPortfolios())
 
 if __name__ == "__main__":
     main()
